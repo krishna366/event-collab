223437ec8f5a5505b3338c452dfdc8cb368e9612
Murali Mallapuram <mmallapu@cisco.com>
2012-09-08 17:00:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventCollab::Application.config.secret_token = 'baae1603d4e89e4e3df41b85857d0469043ee52bdd2fdfd0e326e8641818b9660a67e61ecf2dc561975120b60c3c595c3ae3d91837ff606e6df376e4e7803771'
